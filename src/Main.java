3bd0422587eadb018c0f29d52c50f12d49e37f38
NataBarsu <khvorostukhina.n@yandex.ru>
2022-06-26 00:54:30 +0300

    modified Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int account = 1200;
-        int addFunds =700;
+        int addFunds = 700;
         int bonus;
-        if (account>1000){
-            bonus =addFunds/100;
+        if (account > 1000) {
+            bonus = addFunds / 100;
         } else {
             bonus = 0;
         }
-        int accountBalance = (account+addFunds+bonus);
-        System.out.println (bonus);
-        System.out.println (accountBalance);
+        int accountBalance = (account + addFunds + bonus);
+        System.out.println(bonus);
+        System.out.println(accountBalance);
     }
 }
